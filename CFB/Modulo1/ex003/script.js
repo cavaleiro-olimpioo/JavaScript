552852bb5a6b77e97334f7b984ce1c6fbb857472
cavaleiro-olimpioo <guilhermearnaldi@gmail.com>
2025-02-27 19:54:45 -0300

    ex003

---
+++ CFB/Modulo1/ex003/script.js
@@ -0,0 +1,10 @@
+//"use strict"
+
+function tst(){
+    if(true){
+        let nome = "Victtor"
+        console.log(`dentro do if do tst: ${nome}`)
+    }
+    console.log(`dentro do tst ${nome}`)
+}
+tst()
