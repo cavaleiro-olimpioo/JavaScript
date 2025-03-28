a66849a7c83c36c40432804245323904e947b845
AppleHunters <guilhermearnaldi@gmail.com>
2025-03-27 21:41:05 -0300

    ex010

---
+++ CFB/Modulos/Modulo1/ex010/script.js
@@ -0,0 +1,9 @@
+let v1 =10
+let v2 ='10'
+let v3=v1===v2
+let v4={none:'Bruno'}
+
+console.log(`Value = ${v1} and type is ${typeof(v1)}`)
+console.log(`Value = ${v2} and type is ${typeof(v2)}`)
+console.log(`Value = ${v3} and type is ${typeof(v3)}`)
+console.log(`Value = ${v4} and type is ${typeof(v4)}`)
